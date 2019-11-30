7e9da62edfa6f4d05aba648fedec1574f802132e
Berry85 <1182346874@qq.com>
2019-11-30 14:23:19 +0800

    END

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.House;
-import com.example.demo.entity.RespBean;
 import com.example.demo.entity.User;
 import com.example.demo.service.HouseService;
 import com.example.demo.service.UserService;
@@ -20,50 +18,14 @@ public class AdminController {
 	@Autowired
 	HouseService houseService;
 
-	//	获取房东列表
-	@RequestMapping(value = "/landlord_list", method = RequestMethod.GET)
-	public List<User> landlord_list() {
+	@RequestMapping(value = "/landlord", method = RequestMethod.GET)
+	public List<User> landlord() {
 		return userService.getAllLandlord();
 	}
 
-	//	获取租客列表
-	@RequestMapping(value = "/tenant_list", method = RequestMethod.GET)
-	public List<User> tenant_list() {
+	@RequestMapping(value = "/tenant", method = RequestMethod.GET)
+	public List<User> tenant() {
 		return userService.getAllTenant();
 	}
 
-	//上锁用户
-	@RequestMapping(value = "/locked", method = RequestMethod.POST)
-	public RespBean TenantLock(Integer uid) {
-		int i = userService.lockedUser(uid);
-		if (i == 1) {
-			return RespBean.ok("已锁");
-		} else {
-			return RespBean.error("上锁失败");
-		}
-	}
-
-	//获取所有房屋列表
-	@RequestMapping(value = "/house_list", method = RequestMethod.GET)
-	public List<House> house_list() {
-		return houseService.getAllHouse();
-	}
-
-	@RequestMapping(value = "/house/edit/{id}", method = RequestMethod.POST)
-	public RespBean HouseEdit(House house) {
-		int i = houseService.EditHouse(house);
-		if (i == 1) {
-			return RespBean.ok("修改成功");
-		}
-		return RespBean.error("修改失败");
-	}
-
-	@RequestMapping(value = "/house/delete/{id}", method = RequestMethod.DELETE)
-	public RespBean HouseDelete(Integer hid) {
-		int i = houseService.DeleteHouse(hid);
-		if (i == 1) {
-			return RespBean.ok("删除成功");
-		}
-		return RespBean.error("删除失败");
-	}
 }
